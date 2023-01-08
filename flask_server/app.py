707c1a11c80b1c98ba80f21ceca6499bd3a4a460
YehorI <skylainer99@gmail.com>
2023-01-09 01:49:07 +0300

    add html file converter

--- flask_server/app.py
+++ flask_server/app.py
@@ -1,6 +1,8 @@
+import os
 import requests
 from flask import Flask, render_template, redirect, url_for
 from forms import GetHTML
+from werkzeug.utils import secure_filename
 import html2md_table
 from config import SECRET_KEY
 
@@ -14,15 +16,27 @@ def index():
     form = GetHTML()
     if form.validate_on_submit():
         markdown_tables = []
-        if form.input_type.data == 0: # link
+
+        # LINK
+        if form.input_type.data == 0:
             r = requests.get(form.text_window.data)
             tables = html2md_table.tables_from_html(r.text)
             for table in tables:
                 markdown_table = html2md_table.html2markdowntable(table)
                 markdown_tables.append(markdown_table)
-        if form.input_type.data == 1: # html
-            pass
-        if form.input_type.data == 2: # text
+
+        # HTML
+        if form.input_type.data == 1:
+            page = form.html_file.data
+            # filename = secure_filename(page.filename)
+            # page.save(os.path.join(app.instance_path, 'uploaded files', filename))
+            tables = html2md_table.tables_from_html(page)
+            for table in tables:
+                markdown_table = html2md_table.html2markdowntable(table)
+                markdown_tables.append(markdown_table)
+
+        # TEXT
+        if form.input_type.data == 2:
             tables = html2md_table.tables_from_html(form.text_window.data)
             for table in tables:
                 markdown_table = html2md_table.html2markdowntable(table)
